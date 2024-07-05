67c98c8d1068cb8437e75287ce918e4a4ab0a09f
Camila <santiagcm2@gmail.com>
2024-07-05 01:42:08 -0300

    Upload setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+print(find_packages(include="lib", where="darkfeed"))
+
+setup(
+    name="darkfeed",
+    version="0.1",
+    author="Camila Santiago",
+    packages=["darkfeed", "darkfeed.lib"],
+    description="A CLI for parser data from Dark Feed",
+    install_requires = ['python-dateutil','requests'],
+    keywords= ['ransomware', 'victims', 'darkfeed', 'dark feed'],
+    entry_points={
+        "console_scripts": [ "darkfeed = darkfeed.main:main"],
+    },
+)
